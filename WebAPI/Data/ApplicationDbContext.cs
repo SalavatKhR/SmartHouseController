ea7a7592f41f72413cd09bc5e617974769bde6c9
SalavatKhR <SalavatKhR@gmail.com>
2022-07-01 13:40:28 +0300

    configure project for authorization configured identity and OpenIddict server, create ApplicationDbContext.cs

---
+++ WebAPI/Data/ApplicationDbContext.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using WebAPI.Models;
+
+namespace WebAPI.Data;
+
+public class ApplicationDbContext: IdentityDbContext<AppUser>
+{
+    public ApplicationDbContext()
+    {
+    }
+    
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options): base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+
+        builder.Entity<AppUser>(entity =>
+        {
+            entity.HasData(
+                new AppUser()
+                {
+                    Id = "5f34130c-2ed9-4c83-a600-e474e8f48bac",
+                    UserName = "user01@gmail.com",
+                    NormalizedUserName = "USER01@GMAIL.COM",
+                    Email = "user01@gmail.com",
+                    NormalizedEmail = "USER01@GMAIL.COM",
+                    ConcurrencyStamp = "37285e0f-b3c2-4a75-85f6-73a3c4c6da29",
+                    PasswordHash = "AQAAAAEAACcQAAAAEED86xKz3bHadNf8B1Hg8t5qNefw4Bq1Kr2q6Jx9Ss/DcRIcUpLiFkDgQZTqUgJThA==", //qWe!123
+                    SecurityStamp = "DKBWMTFC7TZQZ6UFNZ5BN5XQNDYUBJYQ,09bd35b0-9c9f-4772-8789-e6d4b9fbe9c4",
+                    EmailConfirmed = true
+                }
+            );
+            entity.HasData(
+                new AppUser()
+                {
+                    Id = "120877ed-84b9-4ed5-9b87-d78965fc4fe0",
+                    UserName = "user02@gmail.com",
+                    NormalizedUserName = "USER02@GMAIL.COM",
+                    Email = "user02@gamil.com",
+                    NormalizedEmail = "USER02@GMAIL.COM",
+                    ConcurrencyStamp = "37285e0f-b3c2-4a75-85f6-73a3c4c6da29",
+                    PasswordHash = "AQAAAAEAACcQAAAAEED86xKz3bHadNf8B1Hg8t5qNefw4Bq1Kr2q6Jx9Ss/DcRIcUpLiFkDgQZTqUgJThA==", //qWe!123
+                    SecurityStamp = "DKBWMTFC7TZQZ6UFNZ5BN5XQNDYUBJYQ,09bd35b0-9c9f-4772-8789-e6d4b9fbe9c4",
+                    EmailConfirmed = true
+                }
+            );
+        });
+    }
+}
---
+++ WebAPI/Startup.cs
@@ -0,0 +1,112 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+using WebAPI.Data;
+
+namespace WebAPI;
+
+public class Startup
+{
+    private readonly IConfiguration _configuration;
+    private readonly IWebHostEnvironment _env;
+
+    public Startup(IConfiguration configuration, IWebHostEnvironment env)
+    {
+        _configuration = configuration;
+        _env = env;
+    }
+
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.AddDbContext<ApplicationDbContext>(options =>
+        {
+            options.UseNpgsql(_configuration.GetConnectionString("DefaultConnection"));
+            options.UseOpenIddict();
+        });
+        services.AddIdentity();
+        services.AddCors(opt =>
+        {
+            opt.AddDefaultPolicy(builder =>
+            {
+                builder
+                    .AllowAnyOrigin()
+                    .AllowAnyHeader()
+                    .AllowAnyMethod();
+            });
+        });
+        services.AddAuthenticationAndJwt(_configuration)
+            .AddAuthorization()
+            .AddOpenIddictServer(_env);
+        services.AddControllers();
+        services.AddEndpointsApiExplorer();
+        services.AddSwaggerGen(option =>
+        {
+            option.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+            {
+                In = ParameterLocation.Header,
+                Description = "Please enter a valid token",
+                Name = "Authorization",
+                Type = SecuritySchemeType.Http,
+                BearerFormat = "JWT",
+                Scheme = "Bearer"
+            });
+            option.AddSecurityRequirement(new OpenApiSecurityRequirement
+            {
+                {
+                    new OpenApiSecurityScheme
+                    {
+                        Reference = new OpenApiReference
+                        {
+                            Type = ReferenceType.SecurityScheme,
+                            Id = "Bearer"
+                        }
+                    },
+                    new string[] { }
+                }
+            });
+            option.SwaggerDoc("v1", new OpenApiInfo
+            {
+                Title = "SmartHouseController API",
+                Version = "v1"
+            });
+        });
+    }
+    
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        if (env.IsDevelopment())
+        {
+            app.UseDeveloperExceptionPage();
+            app.UseSwagger();
+            app.UseSwaggerUI(options =>
+            {
+                options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+                options.RoutePrefix = "swagger";
+            });
+        }
+        app
+            .UseStaticFiles() //for wwwroot
+            .UseRouting()
+            .UseAuthentication()
+            .UseAuthorization()
+            .UseCors()
+            .UseEndpoints(endpoints =>
+            {
+                endpoints.MapDefaultControllerRoute();
+            });
+    }
+
+    public static IHostBuilder CreateHostBuilder(string[] args)
+    {
+        return Host
+            .CreateDefaultBuilder(args)
+            .ConfigureAppConfiguration((context, builder) => builder
+                .AddJsonFile("appsettings.json", false, true)
+                .AddJsonFile($"appsettings.{context.HostingEnvironment.EnvironmentName}.json", true)
+                .AddEnvironmentVariables())
+            .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); });
+    }
+    public static void RunApp(string[] args)
+    {
+        CreateHostBuilder(args).Build().Run();
+    }
+}
