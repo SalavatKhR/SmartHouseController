f122ca54073555e7018469f18c10263879bbd42a
SalavatKhR <SalavatKhR@gmail.com>
2022-07-01 12:55:43 +0300

    Authorization init

---
+++ WebAPI/Controllers/AuthorizationController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers;
+
+[ApiController]
+[Route("api/auth")]
+public class AuthorizationController: ControllerBase
+{
+    [HttpGet("test")]
+    public IActionResult TestAction(string data)
+    {
+        return Ok(data);
+    }
+}
