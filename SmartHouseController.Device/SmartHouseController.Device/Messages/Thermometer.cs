e71077bae5c1c0cce030755aedb10f65d1d3582b
SalavatKhR <SalavatKhR@gmail.com>
2022-07-12 10:27:43 +0300

    device init

---
+++ SmartHouseController.Device/SmartHouseController.Device/Messages/Thermometer.cs
@@ -0,0 +1,12 @@
+﻿namespace SmartHouseController.Device.Messages;
+
+public class Thermometer
+{
+    public int battery { get; set; }
+    public double humidity{ get; set; }
+    public int linkquality{ get; set; }
+    public int power_outage_count{ get; set; }
+    public double pressure{ get; set; }
+    public double temperature{ get; set; }
+    public int voltage{ get; set; }
+}
