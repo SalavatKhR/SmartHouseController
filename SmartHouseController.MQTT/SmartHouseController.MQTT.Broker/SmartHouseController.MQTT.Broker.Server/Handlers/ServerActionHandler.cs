deb84317e92c6692246c38d83103b1304a50ae00
SalavatKhR <SalavatKhR@gmail.com>
2022-07-12 09:56:52 +0300

    refactor: divided into methods divided ServerActionHandler.cs into methods

--- SmartHouseController.MQTT/SmartHouseController.MQTT.Broker/SmartHouseController.MQTT.Broker.Server/Handlers/ServerActionHandler.cs
+++ SmartHouseController.MQTT/SmartHouseController.MQTT.Broker/SmartHouseController.MQTT.Broker.Server/Handlers/ServerActionHandler.cs
@@ -17,24 +17,36 @@ public static class ServerActionHandler
         {
             var payload = Encoding.UTF8.GetString(e.ApplicationMessage!.Payload);
             var date = DateOnly.FromDateTime(DateTime.Now);
-            var existingRecord = await db.Statistics.FirstOrDefaultAsync(r => r.date == date && r.topic == topic);
-            if (existingRecord != null)
-                existingRecord.payload = payload;
-            else
-                await db.AddAsync(new Staistics
-                {
-                    date = date,
-                    topic = topic,
-                    payload = payload
-                });
 
-            var oldRecords = await db.Statistics.Where(r =>
-                r.date.AddMonths(1) <= date
-            ).ToListAsync();
-    
-            db.RemoveRange(oldRecords);
-    
+            await AddOrReplaceRecordAsync(topic, payload, date, db);
+
+            await RemoveOldRecordAsync(date, db);
+            
             await db.SaveChangesAsync();
         }
+    }
+
+    private static async Task AddOrReplaceRecordAsync(string topic, string payload, DateOnly date, ApplicationDbContext db)
+    {
+        var existingRecord = await db.Statistics.FirstOrDefaultAsync(r => r.date == date && r.topic == topic);
+       
+        if (existingRecord != null)
+            existingRecord.payload = payload;
+        else
+            await db.AddAsync(new Staistics
+            {
+                date = date,
+                topic = topic,
+                payload = payload
+            });
+    }
+
+    private static async Task RemoveOldRecordAsync(DateOnly date, ApplicationDbContext db)
+    {
+        var oldRecords = await db.Statistics.Where(r =>
+            r.date.AddMonths(1) <= date
+        ).ToListAsync();
+        
+        db.RemoveRange(oldRecords);
     }
 }
